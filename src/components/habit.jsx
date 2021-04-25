f9b253964e202cb49eb8636c1fe2fdcc6cfbe9f5
devseyoung <devseyoung@gmail.com>
2021-04-25 15:21:41 +1000

    created state object

---
+++ src/components/habit.jsx
@@ -0,0 +1,37 @@
+/* eslint-disable no-unreachable */
+import React, { Component } from 'react';
+
+class Habit extends Component {
+  state = {
+    count: 0,
+  };
+  handleIncreament = () => {
+    // state 오브젝트 안에 있는 count를 증가 한 뒤 state를 업데이트 해야함.
+    this.setState({ count: this.state.count + 1 });
+  }
+  handleDecreament = () => {
+    // state 오브젝트 안에 있는 count를 증가 한 뒤 state를 업데이트 해야함.
+    const count = this.state.count - 1;
+    // 0 보다 작으면 0 출력하고 아니면 count 출력해 !!
+    this.setState({ count: count < 0 ? 0 : count });
+  }
+  render() {
+    return (
+    <li className="habit">
+      <span className="habit-name">Reading</span>
+      <span className="habit-count">{this.state.count}</span>
+      <button className="habit-button habit-increase" onClick={this.handleIncreament}>
+        <i className="fas fa-plus-square"></i>
+      </button>
+      <button className="habit-button habit-decrease" onClick={this.handleDecreament}>
+        <i className="fas fa-minus-square"></i>
+      </button>
+      <button className="habit-button habit-delete">
+        <i className="fas fa-trash"></i>
+      </button>
+    </li>
+    );
+  }
+}
+
+export default Habit;
